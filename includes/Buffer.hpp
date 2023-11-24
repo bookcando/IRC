ab36cfcb96fb95d67b34b2ebebb849b88cf0ffb8
hyojong <bluedog129@gmail.com>
2023-11-24 20:25:12 +0900

    feat(Buffer) : implement Buffer

---
+++ includes/Buffer.hpp
@@ -0,0 +1,24 @@
+#ifndef BUFFER_HPP
+#define BUFFER_HPP
+
+#include "./utils/Headers.hpp"
+
+class Buffer {
+private:
+    static BufferMap _bufferForRead;
+    static BufferMap _bufferForWrite;
+public:
+    static void resetReadBuffer(int fd);
+    static void resetWriteBuffer(int fd);
+    static void eraseReadBuffer(int fd);
+    static void eraseWriteBuffer(int fd);
+    static std::string getReadBuffer(int fd);
+    static std::string getWriteBuffer(int fd);
+    static int readMessage(int fd, intptr_t data);
+    static int sendMessage(int fd);
+    static int sendMessage(int fd, std::string message);
+    static void setReadBuffer(std::pair<int, std::string> val);
+    static void setWriteBuffer(std::pair<int, std::string> val);
+};
+
+#endif
