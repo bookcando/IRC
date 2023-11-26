f93439a28ec9f2078562d32d1d77b69dbef759ae
hyojong <bluedog129@gmail.com>
2023-11-26 15:31:18 +0900

    feat : add Client method

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -3,6 +3,7 @@
 
 #include "./utils/Headers.hpp"
 #include "./utils/Containers.hpp"
+#include "./Channel.hpp"
 #include "./Lists.hpp"
 
 // IRC 서버의 클라이언트. 여러 정보를 저장할 예정
@@ -28,15 +29,14 @@ public :
     void resetReadBuffer();
     void resetWriteBuffer();
 
+	void setPassConnect(int flag);
     void setNickname(std::string nickname);
     void setRealname(std::string realname);
     void setHost(std::string host);
     void setClientFd(int clientFd);
     void setUsername(std::string username);
     void setServer(std::string server);
-    void setTime(std::string time);
-    void setRecvBuffer(std::string recvBuffer);
-    void setSendBuffer(std::string sendBuffer);
+    void setTime();
 
 	void addJoinList(Channel* channel);
 	void deleteJoinList(Channel* channel);
@@ -45,22 +45,13 @@ public :
 	int getPassConnect() const;
 	int getClientFd() const;
 	bool getOperator() const;
-	std::string const& getHostname() const;
+	std::string const& getHost() const;
 	std::string const& getNickname() const;
 	std::string const& getRealname() const;
 	std::string const& getUsername() const;
 	std::string const& getServer() const;
 	time_t const& getTime() const;
 	in_addr const& getAddr() const;
-
 };
-// 클라이언트의 정보 : 소켓, 포트, fd, 주소
-// 클라이언트 유저 정보 : 닉네임, 유저네임, 리얼네임, 모드, (접속한) 채널
 
 #endif
-
-
-
-// int Clinet::getUserStatus() {
-//     return _status;
-// }
--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -2,36 +2,114 @@
 #include "../includes/utils/Containers.hpp"
 #include "../includes/Client.hpp"
 
-Client::Client(int clientFd, in_addr info) : _clientFd(clientFd), _nickname(""), _username(""), _realname(""), _recvBuffer(""), _sendBuffer("") {
+Client::Client(int clientFd, in_addr info) : _passConnect(0), _operator(false), _finalTime(time(NULL)), _clientFd(fd), _info(info), _host(inet_ntoa(info)), _nickname(""), _realname(""), _servname("") {
 }
 
 Client::~Client() {
+	ChannelMap::iterator it = _joinList.begin();
+
+	for (; it != _joinList.end(); it++) {
+		if (it->second->getChanOp() != NULL && it->second->getChanOp()->getClientFd() == _clientFd)
+			it->second->setChanOp(NULL);
+		it->second->delClientList(this);
+		it->second->delInviteList(this);
+	}
+	close(_clientFd);
 }
 
-void Client::resetReadBuffer() {
-    _recvBuffer.clear();
+void Client::addJoinList(Channel* channel) {
+	if (_joinList.find(channel->getChannelName()) == _joinList.end())
+		// _joinList.insert(std::make_pair(channel->getChannelName(), channel)); 대체 왜 안 되는 거지?
+		_joinList[channel->getChannelName()] = channel;
 }
 
-void Client::resetWriteBuffer() {
-    _sendBuffer.clear();
+void Client::deleteJoinList(Channel* channel) {
+	if (_joinList.find(channel->getChannelName()) != _joinList.end())
+		_joinList.erase(channel->getChannelName());
 }
 
-std::string Client::getRecvBuffer() {
-    return _recvBuffer;
+int Client::joinChannel(Channel* channel, std::string const& key) {
+	int channelMode = channel->getMode();
+
+	if (_joinList.size() == CHANNEL_LIMIT_PER_USER)
+		return TOOMANYCHANNELS;
+	if (channelMode & INVITE_CHANNEL && !channel->isClientInvite(this))
+		return INVITEONLYCHAN;
+	if (channelMode & USER_LIMIT_PER_CHANNEL && static_cast<size_t>(channel->getUserLimit()) < channel->getUserList().size())
+		return CHANNELISFULL;
+	if (channelMode & KEY_CHANNEL && key != channel->getKey())
+		return BADCHANNELKEY;
+	channel->addClientList(this);
+	if (channelMode & INVITE_CHANNEL)
+		channel->deleteInviteList(this);
+	addJoinList(channel);
+	return IS_SUCCESS;
 }
 
-std::string Client::getSendBuffer() {
-    return _sendBuffer;
+void Client::setPassConnect(int flag) {
+	_passConnect |= flag;
 }
 
-void Client::setRecvBuffer(std::string buffer) {
-    _recvBuffer = buffer;
+void Client::setNickname(std::string nick) {
+	_nickname = nick;
 }
 
-void Client::setSendBuffer(std::string buffer) {
-    _sendBuffer = buffer;
+void Client::setRealname(std::string real) {
+	_realname = real;
 }
 
-int Client::getClientFd() {
-    return _clientFd;
+void Client::setHost(std::string host) {
+	_host = host;
+}
+
+void Client::setUsername(std::string user) {
+	_username = user;
+}
+
+void Client::setServer(std::string serv) {
+	_server = serv;
+}
+
+void Client::setTime() {
+	_finalTime = time(NULL);
+}
+
+bool Client::getOperator() const {
+	return _operator;
+}
+
+int Client::getPassConnect() const {
+	return _passConnect;
+}
+
+int Client::getClientFd() const {
+	return _clientFd;
+}
+
+std::string const& Client::getHost() const {
+	return _host;
+}
+
+std::string const& Client::getNickname() const {
+	return _nickname;
+}
+
+std::string const& Client::getRealname() const {
+	return _realname;
+}
+
+std::string const& Client::getUsername() const {
+	return _username;
+}
+
+std::string const& Client::getServer() const {
+	return _server;
+}
+
+time_t const& Client::getTime() const {
+	return _finalTime;
+}
+
+in_addr const& Client::getAddr() const {
+	return _info;
 }
